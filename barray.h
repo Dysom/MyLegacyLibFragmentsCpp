66750904ef1bf334a0bbddb62f8dc877bef2a857
Denis Somov <dysom@yandex.ru>
2024-03-11 20:51:07 +0300

    remove comments

--- barray.h
+++ barray.h
@@ -1,15 +1,3 @@
-/*
-* To change this license header, choose License Headers in Project Properties.
-* To change this template file, choose Tools | Templates
-* and open the template in the editor.
-*/
-
-/*
-* File:   barray.h
-* Author: ned
-*
-* Created on November 10, 2017, 12:49 PM
-*/
 
 #ifndef BARRAY_H
 #define BARRAY_H
